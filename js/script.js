f5f5563fcf10fe2a1d8f235ceef38cacf779b0c9
Nicholas Engelbert <nicholas.ge1301@gmail.com>
2022-06-24 17:55:21 -0300

    progress

--- js/script.js
+++ js/script.js
@@ -15,18 +15,27 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('click', keyPress));
 
 function keyPress(event) {
-  console.log(event.target.innerText);
-  if (event.target.classList.contains('backspace')) {
+  const letter = event.target.innerText; // pode ser 'ENTER' ou ''
+  const activeSquare = getActiveSquare();
+  if (+activeSquare.id % 5 === 0) {
+    // ultimo press foi enter?
+    // entao ta livre pra escrever qualquer letra
+
+    // se não, a única tecla que pode ser apertada é enter
+
+  }
+
+  if (event.target.id === 'backspace' || event.target.nodeName === 'IMG') {
     eraseLetter();
     return;
   }
 
-  if (event.target.classList.contains('enter')) {
+  if (event.target.id === 'enter') {
     enterWord();
     return;
   }
 
-  const activeSquare = getActiveSquare();
+
   activeSquare.innerText = event.target.innerText;
 }
 
@@ -48,5 +57,27 @@ function eraseLetter() {
 }
 
 function enterWord() {
+  const firstEmpty = getActiveSquare();
+  if (+firstEmpty.id % 5 !== 0) {
+    alert('not enough letters');
+    return;
+  }
 
+  const guess = getGuess();
+  if (!validWordList.includes(guess)) {
+    console.log(guess);
+    alert('not a valid word');
+    return;
+  }
+}
+
+function getGuess() {
+  const firstEmpty = getActiveSquare();
+  const firstEmptyId = firstEmpty.id;
+  let guess = '';
+  for (let i = firstEmptyId - 5; i < firstEmptyId; i++) {
+    const square = document.getElementById(`${i}`);
+    guess += square.innerText;
+  }
+  return guess;
 }
